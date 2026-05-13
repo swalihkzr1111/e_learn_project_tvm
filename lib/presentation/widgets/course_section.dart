acbdf3019989a90de8ed20e451eee031a7ff0841
swalihkzr1111 <mswalih0999@gmail.com>
2026-05-13 14:32:39 +0530

    implemented filter option

--- lib/presentation/widgets/course_section.dart
+++ lib/presentation/widgets/course_section.dart
@@ -3,19 +3,23 @@ import 'package:e_learn/data/mock/mock_course_data.dart';
 import 'package:e_learn/presentation/widgets/course_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/home_providers.dart';
 
 class CourseSection extends StatelessWidget {
   const CourseSection({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<HomeProvider>(context);
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.w),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            "All Courses",
+            "${provider.selectedCategory} Courses",
             style: TextStyle(
               color: Colors.black,
               fontSize: 16.sp,
@@ -26,7 +30,7 @@ class CourseSection extends StatelessWidget {
           GridView.builder(
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
-            itemCount: mockCourses.length,
+            itemCount: provider.filteredCourses.length,
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
               crossAxisSpacing: 12.w,
@@ -34,7 +38,7 @@ class CourseSection extends StatelessWidget {
               childAspectRatio: 0.60,
             ),
             itemBuilder: (context, index) {
-              final course = mockCourses[index];
+              final course = provider.filteredCourses[index];
               return CourseCard(course: course, index: index);
             },
           ),
